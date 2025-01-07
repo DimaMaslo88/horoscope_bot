d1f3e0b56ce486dec8c353b59f739a709be20273
DimaMaslo88 <dmitrij-maslo@mail.ru>
2025-01-07 14:58:28 +0300

    "feat/ bot logic"

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-const telegramApi = require('node-telegram-bot-api')
 
+
+const telegramApi = require('node-telegram-bot-api')
+const  {gameOptions,againOptions}  = require( './options')
 
 const token = '7118715362:AAFD29pZps2ve4rDlAaAomZGOmV_L4h6JCI'
 
 const bot = new telegramApi(token, {polling: true})
 const data = {}
-const gameOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
 
-            [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
-            [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
-            [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
-            [{text: '0', callback_data: '0'}],
-
-        ]
-    })
-}
-const againOptions ={
-    reply_markup: JSON.stringify({
-        inline_keyboard:[
-           [ {text:'Играть еще раз', callback_data:'/again'}]
-        ]
-    })
-}
 const startGame = async (chatId)=>{
     await bot.sendMessage(chatId, 'Давай сыграем в игру! Я загадаю цифру от от 1 до 10. Угадай ее')
     const randomNumber = Math.floor(Math.random() * 10)
